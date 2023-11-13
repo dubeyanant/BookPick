3b4b0f13f3376c53e044e30f9bc06143ea88b4f3
dubeyanant <anantkumar312@gmail.com>
2023-11-14 02:28:48 +0530

    style(Footer): add background color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,17 @@ import { GithubFilled, TwitterCircleFilled } from "@ant-design/icons";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between">
-      <p>Made with ❤️ by Anant Dubey</p>
-      <div>
-        <a href="https://github.com/dubeyanant/BookPick" className="mr-3">
-          <GithubFilled style={{ fontSize: "24px" }} />
-        </a>
-        <a href="https://twitter.com/_aanant">
-          <TwitterCircleFilled style={{ fontSize: "24px" }} />
-        </a>
+    <div className="bg-gray-200">
+      <div className="flex justify-between container mx-auto px-5 py-2">
+        <p>Made with ❤️ by Anant Dubey</p>
+        <div>
+          <a href="https://github.com/dubeyanant/BookPick" className="mr-3">
+            <GithubFilled style={{ fontSize: "24px" }} />
+          </a>
+          <a href="https://twitter.com/_aanant">
+            <TwitterCircleFilled style={{ fontSize: "24px" }} />
+          </a>
+        </div>
       </div>
     </div>
   );
