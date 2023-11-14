7c263d8f2c39b8146eac8285ffd49caeabcdefc5
dubeyanant <anantkumar312@gmail.com>
2023-11-14 11:28:11 +0530

    style(Home): change card css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,15 @@ const Home = () => {
               alt={item["Book-Title"]}
               className="object-cover h-72 rounded"
             />
-            <p className="font-bold text-xl mt-3">{item["Book-Title"]}</p>
-            <p>{item["Book-Author"]}</p>
-            <p>Votes: {item["num_rating"]}</p>
-            <p>Rating: {parseFloat(item["avg_rating"]).toFixed(2)}</p>
+            <p className="font-bold text-xl mt-3 mr-5">{item["Book-Title"]}</p>
+            <p className="font-medium text-lg">{item["Book-Author"]}</p>
+            <p>
+              Votes: <span className="font-medium">{item["num_rating"]}</span>{" "}
+              &#8226; Rating:{" "}
+              <span className="font-medium">
+                {parseFloat(item["avg_rating"]).toFixed(2)}
+              </span>
+            </p>
           </div>
         ))}
       </div>
